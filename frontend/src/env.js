5967fa79ddc7131c78527f84dc34bf6ea5226233
Vinayak Agrawal <agrawalvinayak084@gmail.com>
2025-11-12 18:31:35 +0530

    env.js

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -11,7 +11,14 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string()
         : z.string().optional(),
-        DATABASE_URL: z.string().url(),
+        DATABASE_URL: z
+        .string()
+        .min(1)
+        .refine(
+          (value) =>
+            value.startsWith("postgres://") || value.startsWith("postgresql://"),
+          { message: "DATABASE_URL must be a valid Postgres connection string" }
+        ),
 
     NODE_ENV: z
       .enum(["development", "test", "production"])
