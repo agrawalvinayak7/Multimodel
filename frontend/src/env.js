3cb382a40d3aa03c25987e02d69d6061c4eecbf1
Vinayak Agrawal <agrawalvinayak084@gmail.com>
2025-11-12 16:09:21 +0530

    database url env bug fix

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -11,7 +11,13 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string()
         : z.string().optional(),
-    DATABASE_URL: z.string().url(),
+        DATABASE_URL: z
+        .string()
+        .min(1)
+        .refine(
+          (v) => v.startsWith("postgres://") || v.startsWith("postgresql://"),
+          { message: "DATABASE_URL must be a valid Postgres connection string" }
+        ),
 
     NODE_ENV: z
       .enum(["development", "test", "production"])
